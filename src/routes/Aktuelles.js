29953b746724de78f328f28eeb6b48d544d5cfc3
Tobias Graf <tobiasgraf87@gmail.com>
2018-07-05 14:22:35 +0200

    Lightbox für Preislist && Aktuelles deaktiviert falls keine Id gegeben

--- src/routes/Aktuelles.js
+++ src/routes/Aktuelles.js
@@ -6,9 +6,15 @@ class Aktuelles extends Component {
     render() {
         return (
             <div align="center">
-                 <FacebookProvider appId={process.env.REACT_APP_APP_ID}>
+            { process.env.REACT_APP_APP_ID && 
+                (<FacebookProvider appId={process.env.REACT_APP_APP_ID}>
                     <Page href="https://www.facebook.com/SalonArtifex"  width={500} height={800} tabs="timeline" />
-                </FacebookProvider>   
+                </FacebookProvider>)
+              
+            }
+            { !process.env.REACT_APP_APP_ID && 
+                (<h3>Oh, da ist etwas schief gegangen :(</h3>)
+            }
             </div>
         )
     }
--- src/routes/Preise.js
+++ src/routes/Preise.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css'; // This only needs to be imported once in your app
 
 
 import schaukasten from './preise/Schaukasten.jpg'
 
 class Preise extends Component {
-
+    constructor(props){
+        super(props)
+        this.state ={
+            isOpen: false
+        }
+    }
     render() {
+        const { isOpen } = this.state;
+
         return (
             <div id="preise" align="center">
                 <table cellSpacing="0" rules="none" border="0">
@@ -25,9 +34,14 @@ class Preise extends Component {
                 </tr>
                 <tr>
                     <td align="left">
-                        <p>
-                           <img src={schaukasten} style={{width: 500}} alt="Preisaushang"/>
-                        </p>
+                        <button className="innershadow" onClick={() => this.setState({ isOpen: true })}>
+                            <img src={schaukasten} style={{width: 500}} alt="Preisaushang"/>
+                        </button>
+                        { isOpen && (
+                            <Lightbox mainSrc={schaukasten}
+                                onCloseRequest={() => this.setState({ isOpen: false })}/>
+                        )}
+                        
                     </td>
                 </tr>
                 <tr>
