89974baaf68744e8a2a278b5502e06ea76aa0adb
Tobias Graf <tobias.graf@4com.de>
2018-11-14 08:36:47 +0100

    Geänderte Betriebszeiten

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -10,23 +10,24 @@ class Home extends Component {
                 <div className="innershadow">
                     <img src={mainImage} alt="salon" border="0" title="Willkommen bei Artifex!" style= {{width :400}} />
                 </div>
-            <p> Dieses Jahr bleibt der Salon durchgehend geöffnet. Unsere Urlaubszeiten: </p>
-            <table>
-                <tbody>
-                    <tr>
-                        <th style={{textAlign: 'left'}}>Franzi:</th><td>28.08.- 12.09.</td>
-                    </tr>
-                    <tr>
-                        <th style={{textAlign: 'left'}}>Lora:</th><td>19.07.- 08.08.</td>
-                    </tr>
-                    <tr>
-                        <th style={{textAlign: 'left'}}>Varina:</th><td>11.06.- 26.06.</td>
-                    </tr>
-                    <tr>
-                        <th style={{textAlign: 'left'}}>Wanda:</th><td>24.07.- 04.08.</td>
-                    </tr>
-                </tbody>
-            </table>
+            <h3 > Unsere Öffnungszeiten ab dem 2. Advent: </h3>
+            <div >
+                Dienstags - Freitags : 10 - 20 Uhr * <br/>
+                Samstags: 10 - 20 Uhr * <br/>
+                <small style={{fontSize: '0.7em'}}>*Mit Terminvereinbarungen</small>
+            </div>
+            <br/>
+            <h3>
+                Wir machen Betriebsurlaub!
+            </h3>
+            <div>
+                Do, 27.12.2018 bis Sa, 05.01.2019 <br/>
+                ist der Salon geschlossen <br/>
+                <small style={{fontSize: '0.7em'}}>Termine für das neue Jahr könnt ihr natürlich
+                <a style={{color: 'black'}} href="https://www.e-cut.de/online-buchung/index.php?salon=tech"> <b style={{fontWeight: 'bold', textDecoration:'underline'}}>hier</b> </a>
+                Buchen!</small>
+            </div>
+
             </div>
         )
     }
