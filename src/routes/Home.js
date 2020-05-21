f99d004fb5f868ff74cda7598ec36317f30a4c68
Techniko86 <62357241+Techniko86@users.noreply.github.com>
2020-05-21 16:32:45 +0200

    Home Coronaupdate
    Changed Home-picture
    Deletet Welcometext

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
+import mainImage from './home/Kundenhinweis2.jpg';
 
-import mainImage from './home/salon_umbau.jpg'
 
 class Home extends Component {
 
     render() {
         return (
-          <div align="center">
-            <h3>Hier d&uuml;rft Ihr Euch wie zu Hause f&uuml;hlen!</h3>
+          <div align="center"> 
+          {/* <h3>Hier dürft Ihr Euch wie zu Hause fühlen!</h3> */}
             <div className="innershadow">
               <img
                 src={mainImage}
@@ -26,7 +26,7 @@ class Home extends Component {
               <tbody>
                 <tr>
                   <th style={{ textAlign: "left" }}>Franzi:</th>
-                  <td>11.08. - 26.08.</td>
+                  <td>08.06. - 26.08.</td>
                 </tr>
                 <tr>
                   <th style={{ textAlign: "left" }}>Varina:</th>
