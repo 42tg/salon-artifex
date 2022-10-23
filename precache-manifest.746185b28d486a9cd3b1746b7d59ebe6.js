ee6b5b811697bfadcc4ebcd8bd7cb620b2e3b6ad
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2022-10-23 08:15:47 +0000

    Deploy 42tg/salon-artifex to github.com/42tg/salon-artifex.git:gh-pages

---
+++ precache-manifest.746185b28d486a9cd3b1746b7d59ebe6.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a11f5833b3055b5b401a100e4a2de0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5572fe8e1d11f4cc4a56",
+    "url": "/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "6804d0aa0edda117ac03",
+    "url": "/static/css/main.bc9e1f85.chunk.css"
+  },
+  {
+    "revision": "5572fe8e1d11f4cc4a56",
+    "url": "/static/js/2.1e492332.chunk.js"
+  },
+  {
+    "revision": "6804d0aa0edda117ac03",
+    "url": "/static/js/main.11af40f7.chunk.js"
+  },
+  {
+    "revision": "3d370066536b5ac39710",
+    "url": "/static/js/runtime-main.e0778bc8.js"
+  },
+  {
+    "revision": "fb7281a471f6cb49fe1ae30ea72783d2",
+    "url": "/static/media/Artifex-Logo-Final.fb7281a4.png"
+  },
+  {
+    "revision": "b488df9d736d9f14ef4f6348deef66cb",
+    "url": "/static/media/Kundenhinweis2.b488df9d.jpg"
+  },
+  {
+    "revision": "22a8009d95d3f227fd0d333bc02cff39",
+    "url": "/static/media/Schaukasteninnen22b.22a8009d.jpg"
+  },
+  {
+    "revision": "b26b84b8ea9ab4337176b7c35e48a411",
+    "url": "/static/media/anfahrt.b26b84b8.jpg"
+  },
+  {
+    "revision": "fac6742f8474565a986a75d61b83b879",
+    "url": "/static/media/filiz.fac6742f.jpg"
+  },
+  {
+    "revision": "7b054fed1b8806b011e993dcceee44a8",
+    "url": "/static/media/franzi.7b054fed.jpg"
+  },
+  {
+    "revision": "4a28bbdab6cd6348adace7bee9b4d12c",
+    "url": "/static/media/gara.4a28bbda.ttf"
+  },
+  {
+    "revision": "b01f0604efa5e7543f1a07532d1576f3",
+    "url": "/static/media/lora.b01f0604.jpg"
+  },
+  {
+    "revision": "8bc4024967c1a5c041621625917d3a2a",
+    "url": "/static/media/movie.8bc40249.jpg"
+  },
+  {
+    "revision": "8e089780849e18989170ae921338ef19",
+    "url": "/static/media/paperback.8e089780.jpg"
+  },
+  {
+    "revision": "9eba3452c3bea6fe915e53a61d6b6b6d",
+    "url": "/static/media/pomadeshop_logo.9eba3452.png"
+  },
+  {
+    "revision": "b91d8604fe9345d12316694586b9fbb1",
+    "url": "/static/media/schwarzkopf.b91d8604.svg"
+  },
+  {
+    "revision": "2372a1c8db1195297a48831c7eae91e3",
+    "url": "/static/media/sh.2372a1c8.png"
+  },
+  {
+    "revision": "228a634d9bc6112e191db4bea5a6e217",
+    "url": "/static/media/team.228a634d.jpg"
+  },
+  {
+    "revision": "867606f2e8bb1ba31e957b1eb2e68a12",
+    "url": "/static/media/varina.867606f2.jpg"
+  },
+  {
+    "revision": "9b19b8914adb01dc1847e556a2e6323a",
+    "url": "/static/media/wanda.9b19b891.jpg"
+  }
+]);
